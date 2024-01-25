d5860dff47b05d84b4b39daee9377d49e1f16426
safon <safonchik.jn@gmail.com>
2024-01-26 00:02:11 +0200

    maybe working (no addresses yet)

--- main.go
+++ main.go
@@ -4,18 +4,29 @@ import (
 	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
+	"strings"
 	"sync"
 )
 
 const (
 	ethereumNodeURL     = "wss://mainnet.infura.io/ws/v3/76e1210cd141441da26a50f5f14735eb"
 	usdtContractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-	transferEventName   = "Transfer"
+	contractAbi         = `[{
+							  "anonymous": false,
+							  "inputs": [
+								{"indexed": true, "name": "from", "type": "address"},
+								{"indexed": true, "name": "to", "type": "address"},
+								{"indexed": false, "name": "tokens", "type": "uint256"}
+							  ],
+							  "name": "Transfer",
+							  "type": "event"
+							}]`
 )
 
 type TransferEvent struct {
@@ -31,34 +42,14 @@ func main() {
 	}
 
 	contractAddress := common.HexToAddress(usdtContractAddress)
-	/*abiBytes, err := os.ReadFile("usdt.abi")
+	usdtAbi, err := abi.JSON(strings.NewReader(contractAbi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	contractAbi, err := abi.JSON(strings.NewReader(string(abiBytes)))
-	if err != nil {
-		log.Fatal(err)
-	}*/
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{contractAddress},
+		Topics:    [][]common.Hash{{usdtAbi.Events["Transfer"].ID}},
 	}
-
-	/*logs, err := client.FilterLogs(context.Background(), query)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, vLog := range logs {
-
-		event := new(TransferEvent)
-		err := contractAbi.UnpackIntoInterface(event, transferEventName, vLog.Data)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("Received USDT Transfer Event: From %s, To %s, Tokens %s\n", event.From.Hex(), event.To.Hex(), event.Tokens.String())
-
-	}*/
 
 	logs := make(chan types.Log)
 	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
@@ -76,15 +67,14 @@ func main() {
 			case err := <-sub.Err():
 				log.Fatal(err)
 			case vLog := <-logs:
-				fmt.Println(vLog)
-				/*event := new(TransferEvent)
-				err := contractAbi.UnpackIntoInterface(event, transferEventName, vLog.Data)
+				event := new(TransferEvent)
+				err := usdtAbi.UnpackIntoInterface(event, "Transfer", vLog.Data)
 				if err != nil {
 					log.Println("Error unpacking log data:", err)
 					continue
 				}
 
-				fmt.Printf("Received USDT Transfer Event: From %s, To %s, Tokens %s\n", event.From.Hex(), event.To.Hex(), event.Tokens.String())*/
+				fmt.Printf("Received USDT Transfer Event: From %s, To %s, Tokens %s\n", event.From.Hex(), event.To.Hex(), event.Tokens.String())
 			}
 		}
 	}()
